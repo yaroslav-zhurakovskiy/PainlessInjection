3960583cab911aa855815bdbe46a942f72dc8d9b
Yaroslav Zhurakovskiy <yaroslav.zhurakovskiy@gmail.com>
2016-07-18 16:51:47 +0300

    Add Pods dir

---
+++ PainlessInjection/Pods/Target Support Files/PainlessInjectionLoader/PainlessInjectionLoader-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_PainlessInjectionLoader : NSObject
+@end
+@implementation PodsDummy_PainlessInjectionLoader
+@end
---
+++ PainlessInjection/Pods/Target Support Files/PainlessInjectionLoader/PainlessInjectionLoader-umbrella.h
@@ -0,0 +1,8 @@
+#import <UIKit/UIKit.h>
+
+#import "PainlessInjectionLoader.h"
+#import "PIModuleLoader.h"
+
+FOUNDATION_EXPORT double PainlessInjectionLoaderVersionNumber;
+FOUNDATION_EXPORT const unsigned char PainlessInjectionLoaderVersionString[];
+
